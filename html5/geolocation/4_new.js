82083f112550bf7afec9471cef79d0ead01cb592
Uerica <minas19901231@gmail.com>
2019-06-17 18:56:14 +0800

    617 jq classroom

---
+++ html5/geolocation/4_new.js
@@ -0,0 +1,19 @@
+function doFirst() {
+  var area = document.getElementById("myMap");
+  // var myPosition = new google.maps.LatLng(24.9774721,121.1824813); //24.9686114,121.1899195
+  var myPosition = new google.maps.LatLng(24.9686114, 121.1899195);
+
+  var map = new google.maps.Map(area, {
+    zoom: 14,
+    center: myPosition,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  });
+
+  var marker = new google.maps.Marker({
+    position: myPosition,
+    map: map,
+    title: "中央大學",
+    icon: "dog.png"
+  });
+}
+window.addEventListener("load", doFirst);
